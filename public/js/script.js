679b50829510780152524462030c339451eb56fa
Mickael <micrandr@gmail.com>
2024-11-10 12:11:34 +0300

    feat: homepage cse 340

---
+++ public/js/script.js
@@ -0,0 +1,2 @@
+const currentYear = document.getElementById('current-year');
+currentYear.append("tesxt");
--- server.js
+++ server.js
@@ -9,11 +9,38 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+var expressLayouts = require('express-ejs-layouts');
+var path = require("path");
+
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+app.set("layout", "./layouts/layout");
+
+app.get("", (req, res)=>{
+  res.render("index", {
+      title: "Home page"
+  })
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+      
+  });
+});
+
+//app.set("views", path.join(__dirname, "views"));
+
+
+
+//app.set('layout extractScripts', true)
+//app.set('layout extractStyles', true)
+
 
 /* ***********************
  * Local Server Information
